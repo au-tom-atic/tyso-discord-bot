0ca16cabe1930c5f5b21074c3efba8c144f1053a
au-tom-atic <trleavy@gmail.com>
2022-05-19 21:27:53 -0700

    testing

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ dotenv.config();
 
 client.once("ready", async () => {
     console.log('discord bot ready to send notifications')
-
-    const notifier = new YouTubeNotifier({
-        hubCallback: `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`,
-        port: process.env.PORT,
-        Secret: 'scoot doo',
-        path: '/youtube'
-      });
-      
-      notifier.setup();
-       
-      notifier.on('notified', data => {
-        console.log(data)
-        client.channels.cache.get(config.discord_channel).send(`${data.channel.name} just uploaded a new video titled: ${data.video.title}. watch it at: ${data.video.link}`)
-      });
-       
-      notifier.subscribe(config.subs);
 });
 
 client.login(process.env.DISCORD_BOT);
+
+const notifier = new YouTubeNotifier({
+  hubCallback: `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`,
+  port: process.env.PORT,
+  Secret: 'scoot doo',
+  path: '/youtube'
+});
+
+notifier.setup();
+ 
+notifier.on('notified', data => {
+  console.log(data)
+  this.client.channels.cache.get(config.discord_channel).send(`${data.channel.name} just uploaded a new video titled: ${data.video.title}. watch it at: ${data.video.link}`)
+});
+ 
+notifier.subscribe(config.subs);
 

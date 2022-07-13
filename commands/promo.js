055637fe5876e804a3bb1bb5adbad883461cfba8
au-tom-atic <trleavy@gmail.com>
2022-07-12 19:22:05 -0700

    testing more

--- commands/promo.js
+++ commands/promo.js
@@ -22,10 +22,10 @@ module.exports = {
             console.log(promos[name])
             const field = {
                 name: name,
-                value: '**pitch**: ' + promos[name][spiel] 
-                + '\n**Offer**: ' + promos[name][offer] 
-                + '\n**Link**: '  + promos[name][link] 
-                + '\n**Code**: '  + promos[name][code] 
+                value: '**pitch**: ' + promos[name].spiel
+                + '\n**Offer**: ' + promos[name].offer
+                + '\n**Link**: '  + promos[name].link
+                + '\n**Code**: '  + promos[name].code
             };
             // add field to fields
             fields.push(field);
